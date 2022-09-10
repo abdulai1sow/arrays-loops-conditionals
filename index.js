fb06b70fff29b7e00fffe08d8378f75b7f483090
sowabdulai <abdulai-as@hotmail.com>
2022-09-10 14:07:28 -0400

    Get Even answered

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 
 //Write a for loop that will log the numbers 1 through 20.
 for (let i = 1; i <= 20; i++){
+  // console.log(i);
+}
+
+// Write a for loop that will log only the even numbers in 0 through 200.
+for (let i = 0; i <= 200; i++) {
+  if(i % 2 === 0)
   console.log(i);
 }
