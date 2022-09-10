eaeeb968742814da78c1beef7c19a867dc26bec4
sowabdulai <abdulai-as@hotmail.com>
2022-09-10 14:34:25 -0400

    Wild Wild Life answered

--- index.js
+++ index.js
@@ -18,19 +18,36 @@ let buzz = 'buzz';
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log(`${fizz} ${buzz}`)
+    // console.log(`${fizz} ${buzz}`)
   }
 }
 //divisible by 3
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0) {
-    console.log(`${i} ${fizz}`)
+    // console.log(`${i} ${fizz}`)
   }
 }
 
 //divisible by 5
 for (let j = 1; j <= 100; j++) {
   if (j % 5 === 0) {
-    console.log(`${j} ${buzz}`)
+    // console.log(`${j} ${buzz}`)
   }
 }
+
+// Wild Wild Life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant", 5000, "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
+
+// Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2]++
+// Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3] = "Gotham City"
+// Give D'Art a second hometown by adding "Hawkins"
+dart.push("Hawkins")
+// Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+wolfy[0] = "Gameboy"
+// console.log(wolfy);
