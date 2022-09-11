2b83c696c7b33f5c45428c6a1361a00d38363e50
sowabdulai <abdulai-as@hotmail.com>
2022-09-10 22:11:36 -0400

    Find the Median answered

--- index.js
+++ index.js
@@ -145,4 +145,14 @@ const kittens = [
 //Rather than hard-coding the array position number set a variable
 let meow = 0
 kittens[meow]
-console.log(meow);
+// console.log(meow);
+
+// ====================Find the Median
+const nums = [14, 11, 16, 15, 13, 16, 15, 17, 19, 11, 12, 14, 19, 11, 15, 17, 11, 18, 12, 17, 12, 71, 18, 15, 12];
+//Find the median number in the following nums array, then console.log that number.
+//hint if you check the length of the array / 2, you might get not get a whole number. In which case, look into Math.floor( // something )
+// let sortNum = nums.sort()
+let median = Math.floor(nums.length/2)//get the median of array
+console.log(median);  
+
+
